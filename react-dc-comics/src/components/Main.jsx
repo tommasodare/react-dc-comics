46c763a2010527407df2cb5837e7308be30979bb
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-20 16:27:56 +0100

    Enhance Main component layout and styling with content and icons

--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -1,11 +1,34 @@
 export default function Main() {
     return (
         <main>
-            <section id="content_box" >
-                <p>Content goes here</p>
+            <section id="content_container">
+                <div id="content_box">
+                    <h1>Content goes here</h1>
+                </div>
             </section>
-            <section id="icon_box" >
-                <p>Ciao</p>
+            <section id="icons_container">
+                <div id="icons_box">
+                    <div className="icon">
+                        <img src="img/buy-comics-digital-comics.png" alt="" />
+                        <p>DIGITAL COMICS</p>
+                    </div>
+                    <div className="icon">
+                        <img src="img/buy-comics-merchandise.png" alt="" />
+                        <p>DC MARCHANDISE</p>
+                    </div>
+                    <div className="icon">
+                        <img src="img/buy-comics-subscriptions.png" alt="" />
+                        <p>SUBSCRIPTIONS</p>
+                    </div>
+                    <div className="icon">
+                        <img width="25px" src="img/buy-comics-shop-locator.png" alt="" />
+                        <p>COMIC SHOP LOCATOR</p>
+                    </div>
+                    <div className="icon">
+                        <img src="img/buy-dc-power-visa.svg" alt="" />
+                        <p>DC POWER VISA</p>
+                    </div>
+                </div>
             </section>
         </main>
     )
