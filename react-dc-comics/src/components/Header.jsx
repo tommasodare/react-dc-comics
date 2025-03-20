ed323edb972e228e9ee549abf717b14d19a4078b
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-20 15:39:20 +0100

    Finish Header and Start Main

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -7,16 +7,16 @@ export default function Header() {
                 </div>
                 <div id="list_box" >
                     <ul>
-                        <li><a href=""></a>Characters</li>
-                        <li><a href=""></a>Comics</li>
-                        <li><a href=""></a>Movies</li>
-                        <li><a href=""></a>TV</li>
-                        <li><a href=""></a>Games</li>
-                        <li><a href=""></a>Collectibles</li>
-                        <li><a href=""></a>Videos</li>
-                        <li><a href=""></a>Fans</li>
-                        <li><a href=""></a>News</li>
-                        <li><a href=""></a>Shop</li>
+                        <li><a href="#"></a>Characters</li>
+                        <li><a href="#"></a>Comics</li>
+                        <li><a href="#"></a>Movies</li>
+                        <li><a href="#"></a>TV</li>
+                        <li><a href="#"></a>Games</li>
+                        <li><a href="#"></a>Collectibles</li>
+                        <li><a href="#"></a>Videos</li>
+                        <li><a href="#"></a>Fans</li>
+                        <li><a href="#"></a>News</li>
+                        <li><a href="#"></a>Shop</li>
                     </ul>
                 </div>
             </div>
--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -1,7 +1,12 @@
 export default function Main() {
     return (
         <main>
-            <p>Ciao Main</p>
+            <section id="content_box" >
+                <p>Content goes here</p>
+            </section>
+            <section id="icon_box" >
+                <p>Ciao</p>
+            </section>
         </main>
     )
 }
