759f253e3eaf8a2545856607a3d0ac574ab49e12
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-20 17:19:42 +0100

    Done first part of Footer

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -1,7 +1,67 @@
 export default function Footer() {
     return (
         <footer>
-            <p>Ciao Footer</p>
+            <section id="main_footer">
+                <div id="container_main_footer" style={{ width: "60%" }}>
+                    <div id="link_box">
+                        <div id="left_list">
+                            <div>
+                                <p>DC COMICS</p>
+                                <ul>
+                                    <li><a href="#"></a>Characters</li>
+                                    <li><a href="#"></a>Comics</li>
+                                    <li><a href="#"></a>Movies</li>
+                                    <li><a href="#"></a>TV</li>
+                                    <li><a href="#"></a>Games</li>
+                                    <li><a href="#"></a>Videos</li>
+                                    <li><a href="#"></a>News</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <p style={{ padding: "1rem 0 1rem 0" }}>SHOP</p>
+                                <ul>
+                                    <li><a href="#"></a>Shop DC</li>
+                                    <li><a href="#"></a>Shop DC Collectibles</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div id="central_list">
+                            <p>DC</p>
+                            <ul>
+                                <li><a href="#"></a>Terms of Use</li>
+                                <li><a href="#"></a>Privacy Police (New)</li>
+                                <li><a href="#"></a>Ad Choises</li>
+                                <li><a href="#"></a>Advertising</li>
+                                <li><a href="#"></a>Jobs</li>
+                                <li><a href="#"></a>Subscriptions</li>
+                                <li><a href="#"></a>Talent Workshops</li>
+                                <li><a href="#"></a>CPSC Certificates</li>
+                                <li><a href="#"></a>Ratings</li>
+                                <li><a href="#"></a>Shop Help</li>
+                                <li><a href="#"></a>Contact Us</li>
+                            </ul>
+                        </div>
+                        <div id="rigth_list">
+                            <p>SITES</p>
+                            <ul>
+                                <li><a href="#"></a>DC</li>
+                                <li><a href="#"></a>MAD Magazine</li>
+                                <li><a href="#"></a>DC Kids</li>
+                                <li><a href="#"></a>DC Universe</li>
+                                <li><a href="#"></a>DC Power Visa</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="logo_box">
+                        <img src="img/dc-logo-bg.png" alt="" />
+                    </div>
+                </div>
+            </section>
+            <section id="second_footer">
+                <div>
+                </div>
+                <div>Follow Us</div>
+            </section>
         </footer>
     )
 }
