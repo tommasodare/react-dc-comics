78226cad21008e79821a1b15d60d079f5825fe34
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-21 15:13:15 +0100

    Add buttons for current series and load more in Main component

--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -5,8 +5,14 @@ export default function Main({ image, title }) {
         <main>
             <section id="content_container">
                 <div id="content_box">
+                    <div className="series_btn">
+                        <button>CURRENT SERIES</button>
+                    </div>
                     <div className="row">
                         <ComicsList />
+                    </div>
+                    <div className="load_btn">
+                        <button>LOAD MORE</button>
                     </div>
                 </div>
             </section>
