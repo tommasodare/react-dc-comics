f43f038dfd5235a421b338f96ee1bcebc842e8f5
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-21 13:56:59 +0100

    Create Layout for Comics Cards

--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -1,9 +1,16 @@
-export default function Main() {
+export default function Main({ image, title }) {
     return (
         <main>
             <section id="content_container">
                 <div id="content_box">
-                    <h1>Content goes here</h1>
+                    <div className="row">
+                        <div className="col">
+                            <div className="comic_box">
+                                <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY" alt="" />
+                                <p>ACTION COMICS</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
