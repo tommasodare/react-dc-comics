d92a1d1ac7fea63218fb8c20cd3397cbc6117bab
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-24 16:36:16 +0100

    Refactor Header component to use menuItems array for navigation links

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,22 +1,30 @@
+const menuItems = [
+    { label: 'Characters', url: '#' },
+    { label: 'Comics', url: '#' },
+    { label: 'Movies', url: '#' },
+    { label: 'TV', url: '#' },
+    { label: 'Games', url: '#' },
+    { label: 'Collectibles', url: '#' },
+    { label: 'Videos', url: '#' },
+    { label: 'Fans', url: '#' },
+    { label: 'News', url: '#' },
+    { label: 'Shop', url: '#' }
+];
+
 export default function Header() {
     return (
         <header>
-            <div id="header_box" >
-                <div id="logo_box" >
+            <div id="header_box">
+                <div id="logo_box">
                     <img src="img/dc-logo.png" alt="" />
                 </div>
-                <div id="list_box" >
+                <div id="list_box">
                     <ul>
-                        <li><a href="#"></a>Characters</li>
-                        <li><a href="#"></a>Comics</li>
-                        <li><a href="#"></a>Movies</li>
-                        <li><a href="#"></a>TV</li>
-                        <li><a href="#"></a>Games</li>
-                        <li><a href="#"></a>Collectibles</li>
-                        <li><a href="#"></a>Videos</li>
-                        <li><a href="#"></a>Fans</li>
-                        <li><a href="#"></a>News</li>
-                        <li><a href="#"></a>Shop</li>
+                        {menuItems.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.url}>{item.label}</a>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
